f9b3aa0d8c49b98fedd24120934baec05eea47e5
hendrik.spiegel <hendrik.spiegel@xing.com>
2017-11-23 18:36:33 +0100

    Added creation of indexes in migration

---
+++ priv/repo/migrations/20171123172452_add_location_indexes.exs
@@ -0,0 +1,15 @@
+defmodule HamburgCityJobs.Repo.Migrations.AddLocationIndexes do
+  use Ecto.Migration
+
+  def up do
+    execute "CREATE INDEX branch_location_index ON branches USING GIST(location);"
+    execute "CREATE INDEX public_transport_location_index ON public_transports USING GIST(location);"
+
+  end
+
+  def down do
+    execute "DROP INDEX IF EXISTS public_transport_location_index;"
+    execute "DROP INDEX IF EXISTS branch_location_index;"
+  end
+  
+end
